c15f54675c37284274b5c1437995fb21846b3618
AJIA <zhujiasheng@users.noreply.github.com>
2020-05-11 12:49:55 +0800

    feat  添加mobxEx 更新webpack配置

--- src/pages/reactEx/index.js
+++ src/pages/reactEx/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.less';
 
+console.log(webp)
 function App() {
-  return <div class="multi-box">
+  return <div className="multi-box">
     react-app
   </div>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@
   const ExtractTextPlugin = require("extract-text-webpack-plugin");
   const VueLoaderPlugin = require('vue-loader/lib/plugin');
   const CopyWebpackPlugin = require('copy-webpack-plugin');
+  const compressionWebpackPlugin = require('compression-webpack-plugin');
 
   const htmlDIR = './src/html';
   const pagesDIR = './src/pages';
@@ -13,8 +14,10 @@
   const files = fs.readdirSync(htmlDIR).map(it => {
     return it.split(".")[0];
   })
+  const _VERSON = +new Date;
+
   const _ExtractTextPlugin = new ExtractTextPlugin({
-    filename:'[name].min.css',
+    filename:`[name].min.css?t=${_VERSON}`,
     disable:false
   })
 
@@ -26,8 +29,10 @@
 
   //output
   output = {
-    filename:`[name].js?t=${+new Date}`,
+    filename:`[name].js?t=${_VERSON}`,
     path:path.resolve(__dirname,'dist'),
+    // library: `${'web'}`,
+    // libraryTarget: "umd"
   }
 
   //plugins
@@ -36,7 +41,10 @@
       template:`${htmlDIR}/${it}.html`,
       filename:`${it}.html`,
       inject:true,
-      chunks:[`${it}`]  
+      chunks:[`${it}`],
+      css:[],
+      js:['js/lodash.js','js/react.js','js/react-dom.js'],
+      webpackjson:'HELLO'
     }));
     return curr;
   },[]);
@@ -46,13 +54,33 @@
   plugins.push(new VueLoaderPlugin());
   plugins.push(new CopyWebpackPlugin([{
     from:path.resolve(__dirname,'static'),
-    to:'static',
+    to:path.resolve(__dirname,'static'),
     // ignore: ['*.ts', '*.less'],
-  }]))
+  },{
+    from:'./node_modules/lodash/lodash.js',
+    to:path.resolve(__dirname,'dist/js/lodash.js')
+  },{
+    from:'./node_modules/react-dom/umd/react-dom.production.min.js',
+    to:path.resolve(__dirname,'dist/js/react-dom.js')
+  },{
+    from:'./node_modules/react/umd/react.production.min.js',
+    to:path.resolve(__dirname,'dist/js/react.js')
+  }]));
+
+  // plugins.push(new webpack.ProvidePlugin({
+  //   cloneDeep:['lodash','cloneDeep'],
+  //   React:['react'],
+  //   ReactDOM:['react-dom']
+  // }))
+
+  plugins.push(new webpack.DefinePlugin({
+    webp:JSON.stringify('webpack')
+  }))
+
+  // plugins.push(new compressionWebpackPlugin())
   // plugins.push(new webpack.HotModuleReplacementPlugin({
   //   // Options...
   // }))
-
   //module
   _module = {
     rules:[
@@ -116,6 +144,12 @@
     module:_module,
     plugins
   }
+
+  config.externals = {
+    'lodash':"lodash",
+    'react-dom':'ReactDOM',
+    'react':'React'
+  }
   
   config.resolve = {
     extensions: ['.ts','.tsx','.js','.jsx','.vue','.less','.scss','.css'],
@@ -135,26 +169,41 @@
   //     '@/pages': resolve('src/pages')
   //   }
   // }
-
-  config.optimization = {
-    splitChunks:{
-      chunks:'all',//async 对异步代码分割 all 对同步和异步代码分割
-      minSize: 300000,//当引入的模块大于30kb才会做代码分割
-      maxSize: 0,//当引入的模块大于maxSize时，会尝试对引入的模块进行二次拆分，一般不用配置
-      minChunks: 1,//当一个模块被至少引入1次，才会做代码分割
-      maxAsyncRequests: 5,//当引入模块10个时，只会将前5个模块进行打包
-      maxInitialRequests: 3,//入口文件引入的模块如果超过3个，只会将前3个模块做代码分割
-      automaticNameDelimiter: '~',//文件连接符
-      name: true,//拆分块的名称，让cacheGroups里面的名字有效
-      cacheGroups:{
-        com:{
-          filename:'[name].js',
-          priority:2,//有限权，当一个模块都符合cacheGroups分组条件，将按照优先权进行分组，priority值越大，优先权越高
-          // reuseExistingChunk:true
-        },
-      }//对符合代码拆分的模块进行一个分类
-    }
-  }
+  // config.optimization = {
+  //   splitChunks:{
+  //     chunks:'all',
+  //     name:true,
+  //     cacheGroups:{
+  //       lodash:{
+  //         name:'chunks-lodash',
+  //         test: (module) => {
+  //           return /lodash/.test(module.context)
+  //         },
+  //         chunks:'initial',
+  //         priority:12
+  //       }
+  //     }
+  //   }
+  // }
+  // config.optimization = {
+  //   splitChunks:{
+  //     chunks:'all',//async 对异步代码分割 all 对同步和异步代码分割
+  //     minSize: 300000,//当引入的模块大于30kb才会做代码分割
+  //     maxSize: 0,//当引入的模块大于maxSize时，会尝试对引入的模块进行二次拆分，一般不用配置
+  //     minChunks: 1,//当一个模块被至少引入1次，才会做代码分割
+  //     maxAsyncRequests: 5,//当引入模块10个时，只会将前5个模块进行打包
+  //     maxInitialRequests: 3,//入口文件引入的模块如果超过3个，只会将前3个模块做代码分割
+  //     automaticNameDelimiter: '~',//文件连接符
+      // name: true,//拆分块的名称，让cacheGroups里面的名字有效
+  //     cacheGroups:{
+  //       com:{
+  //         filename:'[name].js',
+  //         priority:2,//有限权，当一个模块都符合cacheGroups分组条件，将按照优先权进行分组，priority值越大，优先权越高
+  //         // reuseExistingChunk:true
+  //       },
+  //     }//对符合代码拆分的模块进行一个分类
+  //   }
+  // }
 
   config.devServer = {
     contentBase:`${htmlDIR}/`,
@@ -163,5 +212,5 @@
     hot:true,
     compress: true
   }
-
+  config.mode = 'development';
   module.exports = config;
